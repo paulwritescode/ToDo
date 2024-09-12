8c9a9bb60972a3ba570dbff37d8bdebf22315789
paul king <paulkingdev7@gmail.com>
2024-09-12 13:31:33 +0300

    GET / POST
    First GET and POST

--- main.go
+++ main.go
@@ -7,8 +7,49 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
-	fmt.Println("Hellow Moon i love marion")
+	fmt.Println("Hellow Moon, I love World")
 	app := fiber.New()
+
+	// Slice to hold todos globally
+	todos := []Todo{}
+
+	// GET Route
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"message": "Hellow World, This is Moon",
+		})
+	})
+
+	// POST Route for creating todos
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		// Parse body into a new Todo struct
+		todo := &Todo{}
+		// todo := new(Todo)
+		if err := c.BodyParser(todo); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request body meli"})
+
+		}
+
+		// Validate body field
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Body is required"})
+		}
+
+		// Assign ID and add to todos slice
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		// Return the newly created todo
+		return c.Status(200).JSON(todos)
+	})
+
+	// Start the server
 	log.Fatal(app.Listen(":4000"))
 }
