c6a2c358d148670a64775d6ed7cc26e71cd7b5a7
paul king <paulkingdev7@gmail.com>
2024-09-12 14:31:01 +0300

    Routes Added
    PATCH , DELETE, UPDATE, POST, GET

--- main.go
+++ main.go
@@ -21,17 +21,15 @@ func main() {
 	todos := []Todo{}
 
 	// GET Route
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{
-			"message": "Hellow World, This is Moon",
-		})
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
 	})
 
 	// POST Route for creating todos
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		// Parse body into a new Todo struct
-		todo := &Todo{}
-		// todo := new(Todo)
+		// todo := &Todo{}
+		todo := new(Todo)
 		if err := c.BodyParser(todo); err != nil {
 			return c.Status(400).JSON(fiber.Map{"error": "Invalid request body meli"})
 
@@ -48,6 +46,30 @@ func main() {
 
 		// Return the newly created todo
 		return c.Status(200).JSON(todos)
+	})
+	// Update path(patch) route
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos[i].Completed = true
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+
+	// Delete path(delete) route
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		for i, todo := range todos {
+			// convert the todo.ID to string since it is an int from the interface
+			if fmt.Sprint(todo.ID) == id {
+				todos = append(todos[:i], todos[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"success": "Todo deleted"})
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
 	})
 
 	// Start the server
